a36f0f2ee10c3716d8a166328fc2d024784e3552
Pedro nunes <pedromarcosnunes@gmail.com>
2025-04-30 12:26:32 -0300

    Pedro

--- projeto/setup/urls.py
+++ projeto/setup/urls.py
@@ -1,24 +1,19 @@
-"""
-URL configuration for setup project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
-from todos.views import todo_list
+from todos import views
+from todos.views import (
+    TodoListView, TodoCreateView, TodoUpdateView, TodoDeleteView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", todo_list)
+    path('', views.login_view, name='login'),  # Página inicial agora é o login
+    path('lista/', TodoListView.as_view(), name='todo_list'),
+    path('create/', TodoCreateView.as_view(), name='todo_create'),
+    path('update/<int:pk>/', TodoUpdateView.as_view(), name="todo_update"),
+    path('delete/<int:pk>/', TodoDeleteView.as_view(), name="todo_delete"),
+    path('servico/', views.todo_servico_view, name='todo_servico'),  # Página de serviços
+    path('login/', views.login_view, name='login'),  # Página de login
+    path('logout/', views.logout_view, name='logout'),  # Logout
+    path('cadastro/', views.cadastro_view, name='cadastro'),  # Cadastro
 ]
